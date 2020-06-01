04057854fa1615eb4aaf8e733e257d490d404b47
Evan <eroelke@gmail.com>
2020-05-31 20:54:29 -0600

    add map previews

--- src/field_screen_effect.c
+++ src/field_screen_effect.c
@@ -16,6 +16,7 @@
 #include "link_rfu.h"
 #include "load_save.h"
 #include "main.h"
+#include "map_preview.h"
 #include "menu.h"
 #include "mirage_tower.h"
 #include "metatile_behavior.h"
@@ -44,7 +45,6 @@ static void task0A_fade_n_map_maybe(u8);
 static void sub_808115C(u8);
 static void FillPalBufferWhite(void);
 static void Task_ExitDoor(u8);
-static bool32 WaitForWeatherFadeIn(void);
 static void task0A_mpl_807E31C(u8 taskId);
 static void Task_WarpAndLoadMap(u8 taskId);
 static void Task_DoDoorWarp(u8 taskId);
@@ -67,7 +67,7 @@ static void FillPalBufferWhite(void)
     CpuFastFill16(RGB_WHITE, gPlttBufferFaded, PLTT_SIZE);
 }
 
-static void FillPalBufferBlack(void)
+void FillPalBufferBlack(void)
 {
     CpuFastFill16(RGB_BLACK, gPlttBufferFaded, PLTT_SIZE);
 }
@@ -101,14 +101,22 @@ void FadeInFromBlack(void)
 
 void WarpFadeOutScreen(void)
 {
-    u8 currentMapType = GetCurrentMapType();
-    switch (GetMapPairFadeToType(currentMapType, GetDestinationWarpMapHeader()->mapType))
+    const struct MapHeader *header = GetDestinationWarpMapHeader();
+    
+    if (header->regionMapSectionId != gMapHeader.regionMapSectionId && MapHasPreviewScreen(header->regionMapSectionId, MPS_TYPE_CAVE))
     {
-    case 0:
         FadeScreen(FADE_TO_BLACK, 0);
-        break;
-    case 1:
-        FadeScreen(FADE_TO_WHITE, 0);
+    }
+    else
+    {
+        switch (GetMapPairFadeToType(GetCurrentMapType(), header->mapType))
+        {
+        case 0:
+            FadeScreen(FADE_TO_BLACK, 0);
+            break;
+        case 1:
+            FadeScreen(FADE_TO_WHITE, 0);
+        }
     }
 }
 
@@ -476,9 +484,9 @@ static bool32 PaletteFadeActive(void)
     return gPaletteFade.active;
 }
 
-static bool32 WaitForWeatherFadeIn(void)
+bool32 WaitForWeatherFadeIn(void)
 {
-    if (IsWeatherNotFadingIn() == TRUE)
+    if (IsWeatherNotFadingIn() == TRUE && ForestMapPreviewScreenIsRunning())
         return TRUE;
     else
         return FALSE;
@@ -1267,3 +1275,4 @@ static void Task_EnableScriptAfterMusicFade(u8 taskId)
         EnableBothScriptContexts();
     }
 }
+
