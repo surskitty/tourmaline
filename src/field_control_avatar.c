a959becf608049527f1c227e3112f24ef4feb87e
LOuroboros <lunosouroboros@gmail.com>
2020-10-13 04:51:47 -0300

    Alternate take on the 2-in-1 Bike This implementation is based on Ketsuban's explanations on why the first take could feel "hacky" and his own proposal. Also thanks to Malin for the extra condition that makes it so you can't cast the bike by just pressing the R.

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -97,6 +97,7 @@ void FieldClearPlayerInput(struct FieldInput *input)
     input->input_field_1_2 = FALSE;
     input->input_field_1_3 = FALSE;
     input->dpadDirection = 0;
+    input->pressedRButton = FALSE;
 }
 
 void FieldGetPlayerInput(struct FieldInput *input, u16 newKeys, u16 heldKeys)
@@ -124,6 +125,9 @@ void FieldGetPlayerInput(struct FieldInput *input, u16 newKeys, u16 heldKeys)
             input->heldDirection = TRUE;
             input->heldDirection2 = TRUE;
         }
+
+        if (newKeys & R_BUTTON)
+            input->pressedRButton = TRUE;
     }
 
     if (forcedMove == FALSE)
@@ -232,6 +236,24 @@ int ProcessPlayerFieldInput(struct FieldInput *input)
         FreezeObjectEvents();
         Debug_ShowMainMenu();
         return TRUE;
+    }
+
+    if (input->pressedRButton && TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_MACH_BIKE | PLAYER_AVATAR_FLAG_ACRO_BIKE))
+    {
+        if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_MACH_BIKE)
+        {
+            gPlayerAvatar.flags -= PLAYER_AVATAR_FLAG_MACH_BIKE;
+            gPlayerAvatar.flags += PLAYER_AVATAR_FLAG_ACRO_BIKE;
+            SetPlayerAvatarTransitionFlags(PLAYER_AVATAR_FLAG_ACRO_BIKE);
+            PlaySE(SE_BIKE_HOP);
+        }
+        else
+        {
+            gPlayerAvatar.flags -= PLAYER_AVATAR_FLAG_ACRO_BIKE;
+            gPlayerAvatar.flags += PLAYER_AVATAR_FLAG_MACH_BIKE;
+            SetPlayerAvatarTransitionFlags(PLAYER_AVATAR_FLAG_MACH_BIKE);
+            PlaySE(SE_BIKE_BELL);
+        }
     }
 
     return FALSE;
