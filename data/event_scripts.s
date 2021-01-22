f3bfdd50eb65cb9d79d49cd88e98245667891e68
surskitty <surskitty@gmail.com>
2021-01-22 17:39:22 -0500

    Move deleter in Fallarbor; move relearner in Lilycove.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -652,6 +652,7 @@ EventScript_SetBrineyLocation_Route109:: @ 8271918
 
 	.include "data/scripts/pkmn_center_nurse.inc"
 	.include "data/scripts/move_reminder.inc"
+	.include "data/scripts/move_deleter.inc"
 	.include "data/scripts/obtain_item.inc"
 	.include "data/scripts/record_mix.inc"
 	.include "data/scripts/pc.inc"
@@ -838,6 +839,7 @@ Common_EventScript_PlayerHandedOverTheItem:: @ 82723E4
 	.include "data/text/pkmn_center_nurse.inc"
 	.include "data/text/mart_clerk.inc"
 	.include "data/text/obtain_item.inc"
+	.include "data/text/move_deleter.inc"
 	.include "data/text/move_reminder.inc"
 	.include "data/text/flying_taxi.inc"
 
