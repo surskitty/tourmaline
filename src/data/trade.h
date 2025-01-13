c8b365fdcb51476a3309eae4788e0c41fe78b014
surskitty <surskitty@gmail.com>
2025-01-13 18:03:25 -0500

    Changing first trade to galarian zigzagoon; battle rosters until first gym.

--- src/data/trade.h
+++ src/data/trade.h
@@ -986,19 +986,19 @@ static const struct InGameTrade sIngameTrades[] =
 {
     [INGAME_TRADE_SEEDOT] =
     {
-        .nickname = _("DOTS"),
-        .species = SPECIES_SEEDOT,
+        .nickname = _("Beatles"),
+        .species = SPECIES_ZIGZAGOON_GALAR,
         .ivs = {5, 4, 5, 4, 4, 4},
-        .abilityNum = 1,
+        .abilityNum = 2,
         .otId = 38726,
         .conditions = {30, 5, 5, 5, 5},
         .personality = 0x84,
         .heldItem = ITEM_CHESTO_BERRY,
         .mailNum = -1,
-        .otName = _("KOBE"),
+        .otName = _("Kobe"),
         .otGender = MALE,
         .sheen = 10,
-        .requestedSpecies = SPECIES_RALTS
+        .requestedSpecies = SPECIES_ZIGZAGOON
     },
     [INGAME_TRADE_PLUSLE] =
     {
