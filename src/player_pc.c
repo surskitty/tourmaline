c5e271c34de5840d1a17f9d92d1498e7697b32c4
surskitty <surskitty@gmail.com>
2025-02-07 02:05:17 -0500

    i definitely test everything before committing

--- src/player_pc.c
+++ src/player_pc.c
@@ -224,7 +224,7 @@ static const struct MenuAction sItemStorage_MenuActions[] =
 
 static const u16 sNewGamePCItems[][2] =
 {
-    { ITEM_POTION, 5 }
+    { ITEM_POTION, 5 },
     { ITEM_NONE, 0 }
 };
 
