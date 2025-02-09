ea7e42fe498864ca6186ab482409eabcbd6a091e
surskitty <surskitty@gmail.com>
2025-02-09 12:33:21 -0500

    Smoothing out Birch speech.

--- src/main_menu.c
+++ src/main_menu.c
@@ -1308,7 +1308,7 @@ static void Task_NewGameBirchSpeech_Init(u8 taskId)
     gTasks[taskId].func = Task_NewGameBirchSpeech_WaitToShowBirch;
     gTasks[taskId].tPlayerSpriteId = SPRITE_NONE;
     gTasks[taskId].data[3] = 0xFF;
-    gTasks[taskId].tTimer = 0xD8;
+    gTasks[taskId].tTimer = 64;
     PlayBGM(MUS_ROUTE122);
     ShowBg(0);
     ShowBg(1);
