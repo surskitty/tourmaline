zacwalton/pokeemerald-expansion

Added up to Basculin and fixed broken palette mons
Corrected the palette references for previous gen 3 additions (wingull, surskit, etc), these now reference the follower sprites. Added surfing mons up to Basulin. Some Gen 5 overworlds are oversized and need polishing up later. Most sprites use follower pallete data. Includes formes but not regional variants.

src/data/object_events/surfable/surfable_pokemon.h
@@ -438,6 +438,595 @@ const struct RideablePokemon gSurfablePokemon[] =
         .species = SPECIES_RAYQUAZA,
         .trainerPose = 0,
     },
+// Gen IV Pokemon
+    {
+        .species = SPECIES_PIPLUP,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_PRINPLUP,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_EMPOLEON,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_BIBAREL,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_RAMPARDOS,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_BUIZEL,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_FLOATZEL,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_SHELLOS_WEST_SEA,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_SHELLOS_EAST_SEA,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_GASTRODON_WEST_SEA,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_GASTRODON_EAST_SEA,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_GARCHOMP,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_MUNCHLAX,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_FINNEON,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_LUMINEON,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_MANTYKE,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_WEAVILE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_LICKILICKY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_RHYPERIOR,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PALKIA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PHIONE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_MANAPHY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_NORMAL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FIGHTING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FLYING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_POISON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_GROUND,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_ROCK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_BUG,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_GHOST,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_STEEL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FIRE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_WATER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_GRASS,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_ELECTRIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_PSYCHIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_ICE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_DRAGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_DARK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FAIRY,
+        .trainerPose = 0,
+    },
+// Gen V
+{
+        .species = SPECIES_OSHAWOTT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DEWOTT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SAMUROTT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HERDIER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_STOUTLAND,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PANPOUR,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SIMIPOUR,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_AUDINO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TYMPOLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PALPITOAD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SEISMITOAD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULIN_RED_STRIPED,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULIN_BLUE_STRIPED,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULIN_WHITE_STRIPED,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TIRTOUGA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CARRACOSTA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DUCKLETT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SWANNA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FRILLISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_JELLICENT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ALOMOMOLA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_AXEW,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FRAXURE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HAXORUS,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CUBCHOO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BEARTIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_STUNFISK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRUDDIGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BOUFFALANT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HYDREIGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_KELDEO,
+        .trainerPose = 0,
+    },
+// Gen VI
+{
+        .species = SPECIES_FROAKIE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FROGADIER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GRENINJA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BUNNELBY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DIGGERSBY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SKIDDO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GOGOAT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PANCHAM,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PANGORO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FURFROU,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SWIRLIX,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SLURPUFF,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BINACLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BARBARACLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SKRELP,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAGALGE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CLAUNCHER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CLAWITZER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HELIOPTILE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HELIOLISK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GOODRA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BERGMITE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_AVALUGG,
+        .trainerPose = 0,
+    },
+// Gen VII
+{
+        .species = SPECIES_POPPLIO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BRIONNE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PRIMARINA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_WISHIWASHI,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_MAREANIE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TOXAPEX,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DEWPIDER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARAQUANID,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_WIMPOD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GOLISOPOD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_NORMAL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FIGHTING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FLYING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_POISON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_GROUND,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_ROCK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_BUG,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_GHOST,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_STEEL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FIRE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_WATER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_GRASS,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_ELECTRIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_PSYCHIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_ICE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_DRAGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_DARK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FAIRY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BRUXISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAMPA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DHELMISE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TAPU_FINI,
+        .trainerPose = 0,
+    },
+// Gen VIII
+{
+        .species = SPECIES_SOBBLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRIZZILE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_INTELEON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CHEWTLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DREDNAW,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CRAMORANT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARROKUDA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BARRASKEWDA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GRAPPLOCT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_OBSTAGOON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CURSOLA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PINCURCHIN,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_EISCUE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCTOZOLT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRACOVISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCTOVISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAKLOAK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAGAPULT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULEGION,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_OVERQWIL,
+        .trainerPose = 0,
+    },
 #ifdef POKEMON_EXPANSION
     {
         .species = SPECIES_KYOGRE_PRIMAL,

surskitty/tourmaline

src/data/object_events/surfable/surfable_pokemon.h
@@ -438,6 +438,595 @@ const struct RideablePokemon gSurfablePokemon[] =
         .species = SPECIES_RAYQUAZA,
         .trainerPose = 0,
     },
+// Gen IV Pokemon
+    {
+        .species = SPECIES_PIPLUP,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_PRINPLUP,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_EMPOLEON,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_BIBAREL,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_RAMPARDOS,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_BUIZEL,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_FLOATZEL,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_SHELLOS_WEST,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_SHELLOS_EAST,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_GASTRODON_WEST,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_GASTRODON_EAST,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_GARCHOMP,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_MUNCHLAX,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_FINNEON,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_LUMINEON,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_MANTYKE,
+        .trainerPose = 0,
+    },
+    {
+        .species = SPECIES_WEAVILE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_LICKILICKY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_RHYPERIOR,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PALKIA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PHIONE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_MANAPHY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_NORMAL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FIGHTING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FLYING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_POISON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_GROUND,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_ROCK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_BUG,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_GHOST,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_STEEL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FIRE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_WATER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_GRASS,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_ELECTRIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_PSYCHIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_ICE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_DRAGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_DARK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCEUS_FAIRY,
+        .trainerPose = 0,
+    },
+// Gen V
+{
+        .species = SPECIES_OSHAWOTT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DEWOTT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SAMUROTT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HERDIER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_STOUTLAND,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PANPOUR,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SIMIPOUR,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_AUDINO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TYMPOLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PALPITOAD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SEISMITOAD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULIN_RED_STRIPED,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULIN_BLUE_STRIPED,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULIN_WHITE_STRIPED,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TIRTOUGA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CARRACOSTA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DUCKLETT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SWANNA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FRILLISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_JELLICENT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ALOMOMOLA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_AXEW,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FRAXURE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HAXORUS,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CUBCHOO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BEARTIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_STUNFISK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRUDDIGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BOUFFALANT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HYDREIGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_KELDEO,
+        .trainerPose = 0,
+    },
+// Gen VI
+{
+        .species = SPECIES_FROAKIE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FROGADIER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GRENINJA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BUNNELBY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DIGGERSBY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SKIDDO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GOGOAT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PANCHAM,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PANGORO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_FURFROU,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SWIRLIX,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SLURPUFF,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BINACLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BARBARACLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SKRELP,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAGALGE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CLAUNCHER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CLAWITZER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HELIOPTILE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_HELIOLISK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GOODRA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BERGMITE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_AVALUGG,
+        .trainerPose = 0,
+    },
+// Gen VII
+{
+        .species = SPECIES_POPPLIO,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BRIONNE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PRIMARINA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_WISHIWASHI,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_MAREANIE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TOXAPEX,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DEWPIDER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARAQUANID,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_WIMPOD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GOLISOPOD,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_NORMAL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FIGHTING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FLYING,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_POISON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_GROUND,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_ROCK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_BUG,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_GHOST,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_STEEL,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FIRE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_WATER,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_GRASS,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_ELECTRIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_PSYCHIC,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_ICE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_DRAGON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_DARK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_SILVALLY_FAIRY,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BRUXISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAMPA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DHELMISE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_TAPU_FINI,
+        .trainerPose = 0,
+    },
+// Gen VIII
+{
+        .species = SPECIES_SOBBLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRIZZILE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_INTELEON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CHEWTLE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DREDNAW,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CRAMORANT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARROKUDA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BARRASKEWDA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_GRAPPLOCT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_OBSTAGOON,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_CURSOLA,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_PINCURCHIN,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_EISCUE,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCTOZOLT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRACOVISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_ARCTOVISH,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAKLOAK,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_DRAGAPULT,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_BASCULEGION,
+        .trainerPose = 0,
+    },
+{
+        .species = SPECIES_OVERQWIL,
+        .trainerPose = 0,
+    },
 #ifdef POKEMON_EXPANSION
     {
         .species = SPECIES_KYOGRE_PRIMAL,