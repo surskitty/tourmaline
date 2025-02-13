7159cb6c4bd1027a9a8a4ca2445cb719ce818b28
surskitty <surskitty@gmail.com>
2025-02-12 19:51:16 -0500

    Simple config changes, not important.

--- include/config/battle.h
+++ include/config/battle.h
@@ -213,7 +213,7 @@
 #define B_RESET_FLAGS_VARS_AFTER_WHITEOUT TRUE // If TRUE, Overworld_ResetBattleFlagsAndVars will reset battle-related Flags and Vars when the player whites out.
 
 // Ingame partner flag
-#define B_SHOW_PARTNER_TARGET             FALSE // Shows the battler partner will target.
+#define B_SHOW_PARTNER_TARGET             TRUE // Shows the battler partner will target.
 
 // Terrain settings
 #define B_TERRAIN_BG_CHANGE         TRUE       // If set to TRUE, terrain moves permanently change the default battle background until the effect fades.
