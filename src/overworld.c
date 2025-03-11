1caf5b36f677fc4d044759827531a6d97676901b
surskitty <surskitty@gmail.com>
2025-03-11 01:12:31 -0400

    Actually fixing overworld encounter levels for realsies probably.

--- src/overworld.c
+++ src/overworld.c
@@ -3538,30 +3538,27 @@ void StartStationaryEncounter(void) {
     // Generate level of pokemon.
     // Applicable flags are sorted by badges, taking into account skipping badges.
     // TODO: fill this out further 
-    if (FLAG_ENTERED_ELITE_FOUR) {
-        level = 60;
-    } else if (FLAG_BADGE05_GET) {
-        level = 25;
-        if (FLAG_RECEIVED_CASTFORM)                 level += 5;
-        if (FLAG_DEFEATED_MAGMA_SPACE_CENTER)       level += 2;
-        if (FLAG_MET_TEAM_AQUA_HARBOR)              level += 2;
-        if (FLAG_GROUDON_AWAKENED_MAGMA_HIDEOUT)    level += 2;
-        if (FLAG_TEAM_AQUA_ESCAPED_IN_SUBMARINE)    level += 2;
-        if (FLAG_BADGE06_GET)                       level += 3;
-        if (FLAG_BADGE07_GET)                       level += 3;
-        if (FLAG_BADGE08_GET)                       level += 3;
-    } else if (FLAG_BADGE01_GET) {
-        level = 10;
-        if (FLAG_DELIVERED_STEVEN_LETTER)   level += 2;
-        if (FLAG_BADGE02_GET)               level += 2;
-        if (FLAG_DOCK_REJECTED_DEVON_GOODS) level += 2;
-        if (FLAG_BADGE03_GET)               level += 3;
-        if (FLAG_BADGE04_GET)               level += 3;
-        if (FLAG_MET_ARCHIE_METEOR_FALLS)   level += 2;
+    if (FlagGet(FLAG_ENTERED_ELITE_FOUR)) {                 level = 60;
+    } else if (FlagGet(FLAG_BADGE05_GET)) {                 level = 25;
+        if (FlagGet(FLAG_RECEIVED_CASTFORM))                level += 5;
+        if (FlagGet(FLAG_DEFEATED_MAGMA_SPACE_CENTER))      level += 2;
+        if (FlagGet(FLAG_MET_TEAM_AQUA_HARBOR))             level += 2;
+        if (FlagGet(FLAG_GROUDON_AWAKENED_MAGMA_HIDEOUT))   level += 2;
+        if (FlagGet(FLAG_TEAM_AQUA_ESCAPED_IN_SUBMARINE))   level += 2;
+        if (FlagGet(FLAG_BADGE06_GET))                      level += 3;
+        if (FlagGet(FLAG_BADGE07_GET))                      level += 3;
+        if (FlagGet(FLAG_BADGE08_GET))                      level += 3;
+    } else if (FlagGet(FLAG_BADGE01_GET)) {             level = 10;
+        if (FlagGet(FLAG_DELIVERED_STEVEN_LETTER))      level += 2;
+        if (FlagGet(FLAG_BADGE02_GET))                  level += 2;
+        if (FlagGet(FLAG_DOCK_REJECTED_DEVON_GOODS))    level += 2;
+        if (FlagGet(FLAG_BADGE03_GET))                  level += 3;
+        if (FlagGet(FLAG_BADGE04_GET))                  level += 3;
+        if (FlagGet(FLAG_MET_ARCHIE_METEOR_FALLS))      level += 2;
     } else {
-        if (FLAG_VISITED_RUSTBORO_CITY)         level = 7; 
-        else if (VAR_PETALBURG_CITY_STATE > 2)  level = 4;
-        else                                    level = 2;
+        if (FlagGet(FLAG_VISITED_RUSTBORO_CITY))        level = 7; 
+        else if (VarGet(VAR_PETALBURG_CITY_STATE) > 2)  level = 4;
+        else                                            level = 2;
     }
     
     // if the Pokemon does not have a held item specified, generate an appropriate berry
